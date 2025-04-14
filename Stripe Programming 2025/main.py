4629fe0dc4d29e7ad5b25ab1cd08902dbf041274
Cytur <cyturgaming@gmail.com>
2025-04-14 10:00:45 -0400

    Main file

---
+++ Stripe Programming 2025/main.py
@@ -0,0 +1,77 @@
+import pygame
+
+# constants
+
+WHITE = [255,255,255]
+BLACK = [0,0,0]
+RED = [255,0,0]
+GREEN = [0,255,0]
+BLUE = [0,0,255]
+
+SCREEN_WIDTH = 840
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH_CENTER = SCREEN_WIDTH / 2
+SCREEN_HEIGHT_CENTER = SCREEN_HEIGHT / 2
+
+PoppinsFont = "Poppins-Medium.ttf"
+
+class TextClass():
+    def __init__(self, textString:str, textFont:pygame.font.Font, textColor:list, textPos:tuple):
+        self.textString = textString
+        self.textFont = textFont
+        self.textRender = textFont.render(textString, True, textColor)
+        self.textPos = textPos
+        self.textRect = None
+        
+    def blit(self):
+        self.textRect = self.textRender.get_rect(center = self.textPos)
+        self.resultText = screen.blit(self.textRender, self.textRect)
+
+class ButtonClass():
+    def __init__(self, btnText:TextClass, btnRect:pygame.Rect, btnLineWidth:int, bgColor:list):
+        self.btnText = btnText
+        self.btnRect = btnRect
+        self.btnLineWidth = btnLineWidth
+        self.bgColor = bgColor
+        
+    def draw(self):
+        self.rectangleRender = pygame.draw.rect(screen, self.bgColor, self.btnRect)
+        self.btnText.blit()
+        
+def CreateButton():
+    
+
+pygame.init()
+pygame.display.set_caption("Animal Journey")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+GameState = "TitleScreen"
+
+RunVar = True
+while RunVar == True:
+    match GameState:
+        case "TitleScreen":
+            screen.fill(WHITE)
+            
+            titleText = TextClass("Animal Journey", pygame.font.Font(PoppinsFont, 50), BLACK, (SCREEN_WIDTH_CENTER, 175))
+            titleText.blit()
+            
+            startButton = ButtonClass(TextClass("Start", pygame.font.Font(PoppinsFont, 20), BLACK, (SCREEN_WIDTH_CENTER, 225)), pygame.Rect(SCREEN_WIDTH_CENTER - 50, 225 - 25, 100, 50), 0, GREEN)
+            startButton.draw()
+            
+        case "EndScreen":
+            screen.fill(WHITE)
+            
+        case _:
+            #default
+            pass
+        
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RunVar = False
+            
+    pygame.display.update()
+    
+pygame.quit()
