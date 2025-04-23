f64f735c0da7f17b89d05ff1135763e2ccfb884a
Vivaan Jaiswal <vivaanjaiswal@outlook.com>
2025-04-22 21:57:00 -0400

    Improved Info Card Class

--- Stripe Programming 2025/main.py
+++ Stripe Programming 2025/main.py
@@ -43,20 +43,25 @@ class ButtonClass():
 
 
 class InfoCard():
-    def __init__(self, txt: TextClass, btn: ButtonClass, Title: str, Description:str, Challenges:str, xcor, ycor):
+    def __init__(self, txt: TextClass, btn: ButtonClass, Title: str, Description:str, Challenges:str, xcor, ycor, icon: pygame.Surface, bg: tuple):
         self.title = Title
         self.des = Description
         self.challenges = Challenges
-        self.width = 100
-        self.height = 200
+        self.width = 200
+        self.height = 400
         self.xcor = xcor - self.width/2
         self.ycor = ycor - self.height/2
         self.txt = txt
+        self.icon = icon
+        self.bg = bg
 
     def show(self, Screen):
-        self.card_rect = pygame.draw.rect(Screen, GREEN, pygame.Rect(self.xcor, self.ycor, self.xcor + self.width, self.ycor+self.height))
-        titleText = self.txt(self.title, pygame.font.Font(PoppinsFont, 20), BLACK, (self.xcor, self.ycor))
+        self.card_rect = pygame.draw.rect(Screen, self.bg, pygame.Rect(self.xcor, self.ycor, self.width, self.height))
+        print(self.ycor, self.xcor)
+        titleText = self.txt(self.title, pygame.font.Font(PoppinsFont, 10), BLACK, (self.xcor + self.width/2, self.ycor + 15))
         titleText.blit()
+        desText = self.txt(self.title, pygame.font.Font(PoppinsFont, 5), BLACK, (self.xcor, self.ycor+30))
+
 
 
 #Enemies or obstacles which will move in a straight path
@@ -117,9 +122,6 @@ pygame.init()
 pygame.display.set_caption("Animal Journey")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-GameState = "TitleScreen"
-
-
 
 bird_frames = []
 turt_frames = []
@@ -141,16 +143,18 @@ turtle = BirdTurtle(50, 50, turt_frames)
 # deer = Deer(x, y)
 
 turtle_info = InfoCard(TextClass, ButtonClass, "Leather-Back Sea Turtle", "The largest sea turtle in the world, one that travels thousands of kilometers",
-                       "Pollution, Sharks, Orcas", SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER)
+                       "Pollution, Sharks, Orcas", SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER, turt_frames[0], OCEANBLUE)
 
 bird_info = InfoCard(TextClass, ButtonClass, "Red Winged Blackbird", "A stocky, red and black bird, and one that is very common in North America.",
-                     " Air pollution, Hawks, Eagles", SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER)
+                     " Air pollution, Hawks, Eagles", SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER, bird_frames[0], SKYBLUE)
 
 obstacle_list = []
 cloud_img_list = ["CloudAsset/Cloud 10.png", "CloudAsset/Cloud 11.png", "CloudAsset/Cloud 12.png"]
 
 end_time_player_animation = 0
 end_time_cloud_animation = 0
+
+GameState = "PlayerChoose"
 
 RunVar = True
 while RunVar == True:
@@ -164,8 +168,9 @@ while RunVar == True:
             startButton = ButtonClass(TextClass("Start", pygame.font.Font(PoppinsFont, 20), BLACK, (SCREEN_WIDTH_CENTER, 225)), pygame.Rect(SCREEN_WIDTH_CENTER - 50, 225 - 25, 100, 50), 0, GREEN)
             startButton.draw()
 
-        case "Player Choose":
+        case "PlayerChoose":
             screen.fill(WHITE)
+            turtle_info.show(Screen=screen)
             
         case "EndScreen":
             screen.fill(WHITE)
