8502136dc66ad20f5ce56612c1ef66a3e504f6d2
Vivaan Jaiswal <vivaanjaiswal@outlook.com>
2025-04-22 21:38:21 -0400

    added info card class, and turtle level and asset

--- Stripe Programming 2025/main.py
+++ Stripe Programming 2025/main.py
@@ -7,7 +7,8 @@ WHITE = [255,255,255]
 BLACK = [0,0,0]
 RED = [255,0,0]
 GREEN = [0,255,0]
-BLUE = [0,0,255]
+SKYBLUE = [170,206,250]
+OCEANBLUE = [1,84,130]
 
 SCREEN_WIDTH = 840
 SCREEN_HEIGHT = 600
@@ -41,9 +42,27 @@ class ButtonClass():
         self.btnText.blit()
 
 
+class InfoCard():
+    def __init__(self, txt: TextClass, btn: ButtonClass, Title: str, Description:str, Challenges:str, xcor, ycor):
+        self.title = Title
+        self.des = Description
+        self.challenges = Challenges
+        self.width = 100
+        self.height = 200
+        self.xcor = xcor - self.width/2
+        self.ycor = ycor - self.height/2
+        self.txt = txt
+
+    def show(self, Screen):
+        self.card_rect = pygame.draw.rect(Screen, GREEN, pygame.Rect(self.xcor, self.ycor, self.xcor + self.width, self.ycor+self.height))
+        titleText = self.txt(self.title, pygame.font.Font(PoppinsFont, 20), BLACK, (self.xcor, self.ycor))
+        titleText.blit()
+
+
 #Enemies or obstacles which will move in a straight path
 class ObstacleClass():
-    def __init__(self, startx: int, starty: int, speed: int, image: pygame.Surface, width: int, height: int):
+    def __init__(self, startx: int, starty: int, speed: int, image: pygame.Surface, width: int, height: int, collision: bool):
+        self.collision = collision
         self.xcor = startx
         self.ycor = starty
         self.speed = speed
@@ -58,23 +77,14 @@ class ObstacleClass():
         self.rect = pygame.Rect(self.xcor, self.ycor, self.width, self.height)
 
 
-class Bird():
-    def __init__(self, xcor:int, ycor:int):
+class BirdTurtle():
+    def __init__(self, xcor:int, ycor:int, frame_list:list):
         self.speed = 10
         self.xcor = xcor
         self.ycor = ycor
-        self.birdRect = pygame.Rect((self.xcor, self.ycor), (256, 256))
-        self.frame_width, self.frame_height = 32, 32
+        self.Rect = pygame.Rect((self.xcor, self.ycor), (288, 288))
 
-        # Loading all bird frames into a list
-        self.frames = []
-        for num in range(8):
-            frame = pygame.image.load(f"BirdAsset/BirdFlying{num+1}.png")
-            frame = pygame.transform.scale(frame, size= (64, 64))
-            frame = pygame.transform.flip(frame, flip_x=True, flip_y=False)
-            self.frames.append(frame)
-
-        
+        self.frames = frame_list
         self.frame_count = 0
         self.current_frame = self.frames[self.frame_count]
 
@@ -93,7 +103,7 @@ class Bird():
             self.ycor += self.speed
 
 
-        self.birdRect = pygame.Rect((self.xcor, self.ycor), (256, 256))
+        self.Rect = pygame.Rect((self.xcor, self.ycor), (256, 256))
 
         direction = ""
 
@@ -107,14 +117,34 @@ pygame.init()
 pygame.display.set_caption("Animal Journey")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-GameState = "BirdLevel"
+GameState = "TitleScreen"
 
 
 
+bird_frames = []
+turt_frames = []
 
-bird = Bird(50, 50)
-# turtle = Turtle(x, y)
+for num in range(8):
+            frame = pygame.image.load(f"BirdAsset/BirdFlying{num+1}.png")
+            frame = pygame.transform.scale(frame, size= (64, 64))
+            frame = pygame.transform.flip(frame, flip_x=True, flip_y=False)
+            bird_frames.append(frame)
+
+for num in range(6):
+            frame = pygame.image.load(f"TurtleAsset/24bit-seaturtle{num+1}.png")
+            frame = pygame.transform.scale(frame, size= (96, 96))
+            frame = pygame.transform.flip(frame, flip_x=True, flip_y=False)
+            turt_frames.append(frame)
+
+bird = BirdTurtle(50, 50, bird_frames)
+turtle = BirdTurtle(50, 50, turt_frames)
 # deer = Deer(x, y)
+
+turtle_info = InfoCard(TextClass, ButtonClass, "Leather-Back Sea Turtle", "The largest sea turtle in the world, one that travels thousands of kilometers",
+                       "Pollution, Sharks, Orcas", SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER)
+
+bird_info = InfoCard(TextClass, ButtonClass, "Red Winged Blackbird", "A stocky, red and black bird, and one that is very common in North America.",
+                     " Air pollution, Hawks, Eagles", SCREEN_WIDTH_CENTER, SCREEN_HEIGHT_CENTER)
 
 obstacle_list = []
 cloud_img_list = ["CloudAsset/Cloud 10.png", "CloudAsset/Cloud 11.png", "CloudAsset/Cloud 12.png"]
@@ -133,6 +163,9 @@ while RunVar == True:
             
             startButton = ButtonClass(TextClass("Start", pygame.font.Font(PoppinsFont, 20), BLACK, (SCREEN_WIDTH_CENTER, 225)), pygame.Rect(SCREEN_WIDTH_CENTER - 50, 225 - 25, 100, 50), 0, GREEN)
             startButton.draw()
+
+        case "Player Choose":
+            screen.fill(WHITE)
             
         case "EndScreen":
             screen.fill(WHITE)
@@ -145,14 +178,14 @@ while RunVar == True:
 
         case "BirdLevel":
             current_player = bird
-            screen.fill(BLUE)
+            screen.fill(SKYBLUE)
             
             current_time = pygame.time.get_ticks()
 
             #Set up backround
             if current_time > end_time_cloud_animation:
                 cloud_img = pygame.image.load(random.choice(cloud_img_list))
-                cloud = ObstacleClass(1000, random.randint(150, 450), random.randint(10, 20), cloud_img, cloud_img.get_width(), cloud_img.get_height())
+                cloud = ObstacleClass(1000, random.randint(150, 450), random.randint(5, 15), cloud_img, cloud_img.get_width(), cloud_img.get_height(), True)
                 end_time_cloud_animation = pygame.time.get_ticks() + 300
                 obstacle_list.append(cloud)
 
@@ -172,7 +205,38 @@ while RunVar == True:
                     del obstacle
                 else:
                     screen.blit(obstacle.image, (obstacle.xcor, obstacle.ycor))
-            screen.blit(bird.current_frame, bird.birdRect)
+            screen.blit(bird.current_frame, bird.Rect)
+
+        case "TurtleLevel":
+            current_player = turtle
+            screen.fill(OCEANBLUE)
+            
+            current_time = pygame.time.get_ticks()
+
+            #Set up backround 
+            # if current_time > end_time_cloud_animation:
+            #     cloud_img = pygame.image.load(random.choice(cloud_img_list))
+            #     cloud = ObstacleClass(1000, random.randint(150, 450), random.randint(5, 15), cloud_img, cloud_img.get_width(), cloud_img.get_height(), True)
+            #     end_time_cloud_animation = pygame.time.get_ticks() + 300
+            #     obstacle_list.append(cloud)
+
+            # for obstacle in obstacle_list:
+            #     obstacle.move()
+                
+
+            if current_time > end_time_player_animation:
+                current_player.animation_update()
+                end_time_player_animation = pygame.time.get_ticks() + 50
+
+
+            #Blit all the objects
+            # for obstacle in obstacle_list:
+            #     obstacle.move()
+            #     if obstacle.xcor < -200:
+            #         del obstacle
+            #     else:
+            #         screen.blit(obstacle.image, (obstacle.xcor, obstacle.ycor))
+            screen.blit(current_player.current_frame, current_player.Rect)
 
 
 
