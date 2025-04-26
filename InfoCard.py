1c0ff612520b8626a9b76d64164dc6b385ebf241
Vivaan Jaiswal <vivaanjaiswal@outlook.com>
2025-04-25 21:36:58 -0400

    migration on info cards

--- InfoCard.py
+++ InfoCard.py
@@ -4,7 +4,7 @@ from UIClasses import TextClass, ButtonClass
 
 
 class InfoCard():
-    def __init__(self, Title: str, Description1:str, Description2, Challenges:str, xcor, ycor, icon: pygame.Surface, bg: tuple, y_offset, screen, gamestatefunc, gamestate: str):
+    def __init__(self, Title: str, Description1:str, Description2, Challenges:str, migrationpattern: str, xcor, ycor, icon: pygame.Surface, bg: tuple, y_offset, screen, gamestatefunc, gamestate: str):
         self.screen = screen
 
         self.title = Title
@@ -12,6 +12,7 @@ class InfoCard():
         self.des1 = Description1
         self.des2 = Description2
         self.challenges = Challenges
+        self.migpat = migrationpattern
 
         self.width = 200
         self.height = 300
@@ -23,9 +24,10 @@ class InfoCard():
         self.icon_y_offset = y_offset
 
         self.iconRect = pygame.Rect(self.xcor, self.ycor + 60 - self.icon_y_offset, 96*2, 96*2)
-        self.playbutton = ButtonClass(TextClass("Start", pygame.font.Font(DesignClass.Fonts["Poppins"], 20), DesignClass.Colors["BLACK"], (self.xcor + self.width/2 - 50, self.ycor + 300 - 25), screen), pygame.Rect(self.xcor, self.ycor + 300 - 50, 100, 50), 0, DesignClass.Colors["GREEN"], screen, gamestatefunc, gamestate)
+        self.playbutton = ButtonClass(TextClass("Start", pygame.font.Font(DesignClass.Fonts["Poppins"], 20), DesignClass.Colors["BLACK"], (self.xcor + self.width/2, self.ycor + 300 - 15), screen), pygame.Rect(self.xcor, self.ycor + 300 - 30, 200, 30), 0, DesignClass.Colors["GREEN"], screen, gamestatefunc, gamestate)
         self.desText1 = TextClass(self.des1, pygame.font.Font(DesignClass.Fonts["Poppins"], 9), DesignClass.Colors["BLACK"], (self.xcor + self.width/2, self.ycor + self.height - 80), self.screen)
         self.desText2 = TextClass(self.des2, pygame.font.Font(DesignClass.Fonts["Poppins"], 9), DesignClass.Colors["BLACK"], (self.xcor + self.width/2, self.ycor + self.height - 70), self.screen)
+        self.migpatText = TextClass(self.migpat, pygame.font.Font(DesignClass.Fonts["Poppins"], 9), DesignClass.Colors["BLACK"], (self.xcor + self.width/2, self.ycor + self.height - 50), self.screen)
         self.titleText = TextClass(self.title, pygame.font.Font(DesignClass.Fonts["Poppins"], 15), DesignClass.Colors["BLACK"], (self.xcor + self.width/2, self.ycor + 15), self.screen)
         
 
@@ -35,5 +37,6 @@ class InfoCard():
         self.titleText.blit()
         self.desText1.blit()
         self.desText2.blit()
+        self.migpatText.blit()
         self.playbutton.draw()
         Screen.blit(self.icon, self.iconRect)
